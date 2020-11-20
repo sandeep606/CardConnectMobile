2ba8b727cda674f52eac8d2d5d2cdb8c14fd2a73
sandeep <sandeep@sandeeps-iMac.local>
2020-11-20 09:43:46 +0530

    minor change

--- www/CardConnectMobileiOS.js
+++ www/CardConnectMobileiOS.js
@@ -5,3 +5,7 @@ var exec = require('cordova/exec');
 module.exports.initialisePayment = function (arg0, success, error) {
     exec(success, error, 'CardConnectMobileiOS', 'initialisePayment', [arg0]);
 };
+
+module.exports.payWithCardDetails = function (arg0, success, error) {
+    exec(success, error, 'CardConnectMobileiOS', 'payWithCardDetails', [arg0]);
+};
